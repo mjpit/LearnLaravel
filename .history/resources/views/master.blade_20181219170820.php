15125b5ec1a2c1daabacc5964b3f42e697ffb15f
Marcel Pit <marcel.pit@takeaway.com>
2018-12-20 10:26:56 +0100

    Chapter 2 complete

--- .history/resources/views/master.blade_20181219170820.php
+++ .history/resources/views/master.blade_20181219170820.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <title> @yield('title') </title>
-    b:
+    
    <link rel="stylesheet" href="css/app.css">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     {{-- <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous"> --}}
